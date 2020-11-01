3075dd6bb5345b45425a1ebaf63771341ae5f1a4
Vincent <vincentvibe3@gmail.com>
2020-10-31 23:25:54 -0400

    initial commit

---
+++ EmergencyFood.py
@@ -0,0 +1,54 @@
+import discord
+import random
+import requests
+#import discordbot_commands
+
+client = discord.Client()
+TOKEN ='NzcyMjU2MzAwMzg5MjM2NzY3.X54BuQ.XCiux6yOfhNowA247YvAWmWwHbE'
+commandPrefix = '$'
+description = '''$'''
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity= discord.Game("Kevin is horny"))
+
+@client.event
+async def on_message(message):
+    commandName = 'say'
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(str(commandPrefix+commandName)):
+        userMessage = message.content[len(commandPrefix+commandName):]
+        await message.channel.send(userMessage)
+
+@client.event
+async def on_message(message):
+    commandName = 'spam'
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(str(commandPrefix+commandName)):
+        while True:
+            userMessage = message.content[len(commandPrefix+commandName):]
+            await message.channel.send(userMessage)
+
+@client.event 
+async def on_message(message):
+    commandName = 'sauce'
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(str(commandPrefix+commandName)):
+        if message.channel.is_nsfw():
+            isEnglish = -1
+            while isEnglish == -1:
+                randomSauce = requests.head('https://nhentai.net/random', allow_redirects=True)
+                siteContent = requests.get(randomSauce.url, allow_redirects=True).text
+                isEnglish = siteContent.find('/language/english')
+            await message.channel.send(randomSauce.url)
+        else: 
+            await message.channel.send("This command can only be used in NSFW channels")
+
+client.run('NzcyMjU2MzAwMzg5MjM2NzY3.X54BuQ.XCiux6yOfhNowA247YvAWmWwHbE')
