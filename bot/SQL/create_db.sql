61a0cd45b652d295ea0a47103ac99d0e60ef4c62
Vincent <vincentvibe3@gmail.com>
2021-02-01 21:07:16 -0500

    create db schema

---
+++ bot/SQL/create_db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users(
+    id TEXT PRIMARY KEY, 
+    rolls INT
+);
+
+CREATE TABLE IF NOT EXISTS deathrolls(
+    name PRIMARY KEY,
+);
+
+CREATE TABLE IF NOT EXISTS standardrolls(
+    name PRIMARY KEY,
+);
+
